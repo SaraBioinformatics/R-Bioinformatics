4fe02e813ee1a2359d2ce8b782bcf61f6ed63d2c
Sarita Maurya <115336007+Saritamaurya1234@users.noreply.github.com>
2024-04-06 15:33:16 +0530

    Add files via upload

---
+++ packages.R
@@ -0,0 +1,22 @@
+
+if (!require("BiocManager", quietly = TRUE))
+  
+install.packages("BiocManager")
+BiocManager::install("edgeR")
+BiocManager::install("csaw")
+BiocManager::install("AllelicImbalance")
+BiocManager::install("bumphunter")
+BiocManager::install("DESeq2")
+BiocManager::install("IRanges")
+BiocManager::install("Rsamtools")
+BiocManager::install("rtracklayer")
+BiocManager::install("sva")
+BiocManager::install("SummarizedExperiment")
+BiocManager::install("VariantAnnotation")
+install.packages("dplyr")
+installed.packages("extRemes")####
+install.packages("forcats")
+installed.packages("magrittr")###
+install.packages("rlang")
+
+
